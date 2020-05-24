9320e8ebe3db1cae7cf1f009f484f6cb8e207323
Dmitry Shuvalov <iamshuvalov@yandex.ru>
2020-05-24 23:43:40 +0300

    feat(server): prettify server responses

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -19,25 +19,39 @@ app.use(router.allowedMethods())
 
 router.get('/tasks', async function (ctx: AppContext) {
   try {
-    const result = await ctx.app.mongo.collection('Tasks').find().toArray()
+    const result = await ctx.app.mongo
+      .collection('Tasks')
+      .find()
+      .map(document => {
+        const { _id, ...data } = document
+        return data
+      })
+      .toArray()
     ctx.body = result
+    ctx.type = 'json'
     ctx.status = 200
   } catch (error) {
     console.log('GET:/tasks', error)
-    ctx.body = error
-    ctx.status = 500
+    ctx.type = 'json'
+    ctx.status = error.status || 500
+    ctx.body = { message: 'Internal Server Error' }
   }
 })
 
 router.post('/task', async function (ctx: AppContext) {
   try {
     const { request } = ctx
+
     await ctx.app.mongo.collection('Tasks').insertOne(request.body)
-    ctx.status = 200
+
+    ctx.type = 'json'
+    ctx.status = 201
+    ctx.body = { message: 'Task was successfully created' }
   } catch (error) {
     console.log('POST:/task', error)
-    ctx.body = error
-    ctx.status = 500
+    ctx.type = 'json'
+    ctx.status = error.status || 500
+    ctx.body = { message: 'Internal Server Error' }
   }
 })
 
